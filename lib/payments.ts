8bbea3e46de18b1e41b23399e70b6e75962965f4
hemanshuypatil <hemanshuypatil@gmail.com>
2024-08-04 14:58:44 +0000

    final

--- lib/payments.ts
+++ lib/payments.ts
@@ -1,3 +1,3 @@
 export const monthlyPlanId: string = "price_1Pk5i9SF80PaWJPI04SFRC1y";
 export const yearlyPlanId: string = "price_1Pk5ipSF80PaWJPIR399nLPT";
-export const maxFreeProjects: number = 3;
+export const maxFreeProjects: number = 2;
